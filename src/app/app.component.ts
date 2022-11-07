fa1d52782ea11a0d1f0088f2d3fe41f72c6e0d17
Patokar Karan <tspatkar@ap.dtcs.local>
2022-11-07 13:07:31 +0530

    Changes for patch-package demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { IActionMapping, ITreeOptions, KEYS, TREE_ACTIONS } from '@circlon/angular-tree-component';
+
+const actionMapping: IActionMapping = {
+  keys: {
+    [KEYS.ENTER]: (tree, node, $event) => alert(`This is ${node.data.name}`)
+  }
+};
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TreeViewApp';
+
+  nodes = [
+    {
+      id: 1,
+      name: 'root1',
+      children: [
+        { id: 2, name: 'child1' },
+        { id: 3, name: 'child2' }
+      ]
+    },
+    {
+      id: 4,
+      name: 'root2',
+      children: [
+        { id: 5, name: 'child2.1' },
+        {
+          id: 6,
+          name: 'child2.2',
+          children: [
+            { id: 7, name: 'subsub' }
+          ]
+        }
+      ]
+    }
+  ];
+
+  options: ITreeOptions = {
+    actionMapping
+  };
 }
